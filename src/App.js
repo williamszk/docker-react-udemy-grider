2e32013026746f005dec4f3a6c32d7f850094b18
williamszk <wllmszk@gmail.com>
2023-05-03 18:35:54 +0000

    fix bug, test fail, must have 'Learn React' in <a> tag

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					I was changed on the feature branch!
+					Learn React
 				</a>
+				<p>This is from the feature branch!</p>
 			</header>
 		</div>
 	);
